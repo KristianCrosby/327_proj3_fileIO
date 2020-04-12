f594eb46885a1d8cd7643ab30b31fc18361ae9c1
Kristian Crosby <you@example.com>
2020-04-11 18:23:37 -0700

    finshed file io

--- FileIO.cpp
+++ FileIO.cpp
@@ -1,24 +1,59 @@
 /*
  * FileReader.cpp
  *
- *  Created on: Oct 8, 2017
- *      Author: keith
+ *  Created on: 4/11/2020
+ *      Author: Kristian Crosby
  */
 #include <iostream>
 #include <fstream>
 #include "../327_proj3_test/includes/FileIO.h"
 #include "../327_proj3_test/includes/constants.h"
+#include <vector>
+#include <iterator>
 
 using namespace std;
 
+const int ERROR_COULD_NOT_OPEN_FILE = -1;
+
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
-	//TODO fill in
+	contents.clear();
+
+	ifstream inFile;
+	inFile.open(filename, ios::in);
+	if( !inFile.is_open()){
+		std::string line;
+
+		while(!inFile.eof()){
+			getline(inFile, line);
+			contents = contents + "" + line;
+	}
+	inFile.close();
+	}else {
+		return ERROR_COULD_NOT_OPEN_FILE;
+	}
+	inFile.close();
+	return SUCCESS;
 }
+
 
 int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
-	//TODO fill in
+	ofstream outFile;
+	outFile.open(filename, ios::out);
+
+	if(outFile.is_open()){
+
+		for(vector<string>::iterator vec = myEntryVector.begin(); vec != myEntryVector.end(); vec++) {
+			outFile << *vec << ' ';
+		}
+		outFile <<'\n';
+		outFile.close();
+	} else {
+		return ERROR_COULD_NOT_OPEN_FILE;
+	}
+	return SUCCESS;
+
 }
 
 
